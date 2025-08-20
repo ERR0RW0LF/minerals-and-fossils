523f2c063a00596d1d3fc9ddf971dc0d9a531109
ERR0RW0LF <finn.woma@gmail.com>
2025-08-20 17:23:34 +0200

    Bump mod version to 1.0.2 and add translucent rendering for cut amethyst blocks

--- src/client/java/com/err0rw0lf/MineralsAndFossilsClient.java
+++ src/client/java/com/err0rw0lf/MineralsAndFossilsClient.java
@@ -1,10 +1,15 @@
 package com.err0rw0lf;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class MineralsAndFossilsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.SMALL_CUT_AMETHYST_BLOCK, RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.MEDIUM_CUT_AMETHYST_BLOCK,RenderLayer.getTranslucent());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BIG_CUT_AMETHYST_BLOCK,RenderLayer.getTranslucent());
 	}
 }
